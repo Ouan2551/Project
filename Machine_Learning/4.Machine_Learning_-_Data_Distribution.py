8c331f9bab3a7d4293371895cb92c0927615bad3
Ouan2551 <Nannaphat2110@gmail.com>
2025-02-09 11:18:34 +0700

    -

--- Machine_Learning/4.Machine_Learning_-_Data_Distribution.py
+++ Machine_Learning/4.Machine_Learning_-_Data_Distribution.py
@@ -7,7 +7,10 @@ x = numpy.random.uniform(0.0, 5.0, 250) # (lowest, highest, count)
 print(x)
 
 # histogram => use for visualize data set
-import matplotlib.pyplot as plt1
-# after "as" is just name whatever you want to use
-plt1.hist(x, 5)
-plt1.show
+import matplotlib.pyplot as plt1 # use plt or plt1 or plt2 or ...
+# after "as" is just name library whatever you want to use
+plt1.hist(x, 5) #(value, bars)
+
+# this graph y-label count of value
+# x-label is value of graph
+plt1.show()
