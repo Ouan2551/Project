f6b405e6c6a1a065f81adecf0ffe0bca27b5a04f
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-03-06 13:54:53 +0700

    -

--- Machine_Learning/python+opencv/learn/work_with_picture/started/1.Read_image.py
+++ Machine_Learning/python+opencv/learn/work_with_picture/started/1.Read_image.py
@@ -3,10 +3,12 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+# path of file picture
+path = r"D:\Important files Nannaphat\coding\Project\Machine_Learning\python+opencv\learn\work_with_picture\started\geeksforgeeks.png"
+
 # use library cv2 to show picture
 # read image not for get to use 'r' in front of directory picture
-img = cv2.imread(r"D:\Important files Nannaphat\coding\Project\Machine_Learning\python+opencv\learn\work_with_picture\started\geeksforgeeks.png"
-                , cv2.IMREAD_COLOR)
+img = cv2.imread(path, cv2.IMREAD_COLOR)
 # structure imshow(title gui window, image array)
 cv2.imshow("image : ", img)
 print(img.shape[:3]) # show size of image
@@ -16,7 +18,7 @@ cv2.waitKey(0) # wait user click to close window
 cv2.destroyAllWindows()
 
 # use library matplotlib to show picture
-img1 = cv2.imread(r"D:\Important files Nannaphat\coding\Project\Machine_Learning\python+opencv\learn\work_with_picture\started\geeksforgeeks.png")
+img1 = cv2.imread(path)
 plt.imshow(img1)
 plt.waitforbuttonpress()
 plt.close('all')
@@ -28,3 +30,13 @@ RGB_convert_img = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
 plt.imshow(RGB_convert_img)
 plt.waitforbuttonpress()
 plt.close('all')
+
+# open in grayscale mode (transform picture color to black and white)
+img2 = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+# just test print shape
+# structure (width, heigh, channels(value RGB) )
+print(img2.shape[:2])
+cv2.imshow('image', img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
