82e48bbc54ec52c1fc0e9d43efc9eccd8c1236b2
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-03-08 22:12:29 +0700

    -

--- Machine_Learning/python+opencv/learn/work_with_picture/2.Image_Precessing/3.Bluring_image.py
+++ Machine_Learning/python+opencv/learn/work_with_picture/2.Image_Precessing/3.Bluring_image.py
@@ -21,3 +21,15 @@ cv2.waitKey(0)
 Gaussian = cv2.GaussianBlur(image, (7, 7), 0)
 cv2.imshow("Gaussian Blur : ", Gaussian)
 cv2.waitKey(0)
+
+# Median Blur 
+median = cv2.medianBlur(image, 5) 
+cv2.imshow('Median Blurring', median) 
+cv2.waitKey(0) 
+
+
+# Bilateral Blur 
+bilateral = cv2.bilateralFilter(image, 9, 75, 75) 
+cv2.imshow('Bilateral Blurring', bilateral) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
