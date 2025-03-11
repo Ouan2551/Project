43c93c472b66059e3ae04079a30d3c10e0520fca
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-03-11 22:44:47 +0700

    -

---
+++ Machine_Learning/python+opencv/learn/work_with_picture/2.Image_Precessing/13.Segmentation_using_Thresholding.py
@@ -0,0 +1,7 @@
+# Thresholding => popular segmentation technic use for
+# separating object from background
+# (in this case use for grayscale picture 8 bit)
+
+# the process divide all pixel input image into 2 group
+# 1) pixel have intensity value lower than threshold
+# 2) pixel have intensity value greater than threshold
