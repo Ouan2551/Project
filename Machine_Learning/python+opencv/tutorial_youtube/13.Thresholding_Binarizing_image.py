089e6b4829f6d7441df66ec50cb6eb947f618915
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-03-18 22:31:10 +0700

    -

--- Machine_Learning/python+opencv/tutorial_youtube/13.Thresholding_Binarizing_image.py
+++ Machine_Learning/python+opencv/tutorial_youtube/13.Thresholding_Binarizing_image.py
@@ -9,11 +9,26 @@ gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("gray", gray_image)
 
 # Thresholding => convert image to binary
+
 # 1) simple thresholding
 threshold, thresh = cv2.threshold(gray_image, thresh=150, maxval=255, type=cv2.THRESH_BINARY)
+# value more than 150 set to 255 (pixel intensity)
 cv2.imshow("simple_thresholding", thresh)
 # inverse
 threshold, thresh_inverse = cv2.threshold(gray_image, thresh=150, maxval=255, type=cv2.THRESH_BINARY_INV)
+# value less than 150 set to 255 (pixel intensity)
 cv2.imshow("simple_thresholding_inverse", thresh_inverse)
+
+# 2) adaptive thresholding
+adaptive = cv2.adaptiveThreshold(gray_image, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C, thresholdType=cv2.THRESH_BINARY, blockSize=11, C=3)
+cv2.imshow("adaptive_thresholding", adaptive)
+adaptive_inverse = cv2.adaptiveThreshold(gray_image, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C, thresholdType=cv2.THRESH_BINARY_INV, blockSize=11, C=3)
+cv2.imshow("adaptive_thresholding_inverse", adaptive_inverse)
+# Gaussian
+adaptive_gaussian = cv2.adaptiveThreshold(gray_image, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C, thresholdType=cv2.THRESH_BINARY, blockSize=11, C=3)
+cv2.imshow("adaptive_thresholding_gaussian", adaptive_gaussian)
+adaptive_inverse_gaussian = cv2.adaptiveThreshold(gray_image, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C, thresholdType=cv2.THRESH_BINARY_INV, blockSize=11, C=3)
+cv2.imshow("adaptive_thresholding_inverse_gaussian", adaptive_inverse_gaussian)
+
 if cv2.waitKey(0) & 0xFF == ord('d'):
     cv2.destroyAllWindows()
