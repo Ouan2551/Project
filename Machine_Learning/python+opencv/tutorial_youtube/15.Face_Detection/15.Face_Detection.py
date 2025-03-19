76241f09b7e3b4f8a2c3aad75e4561927808b219
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-03-19 14:44:35 +0700

    -

---
+++ Machine_Learning/python+opencv/tutorial_youtube/15.Face_Detection/15.Face_Detection.py
@@ -0,0 +1,6 @@
+# follow this url for haarcascades
+# https://github.com/opencv/opencv/tree/4.x/data/haarcascades
+
+import cv2
+path = r'D:\Important files Nannaphat\coding\Project\Machine_Learning\python+opencv\tutorial_youtube\Photos\lady.jpg'
+image = cv2.imread(path)
