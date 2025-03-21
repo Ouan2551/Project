f8933c421e70ad874e6ff2d2420b72aba3700f6c
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-03-21 23:34:52 +0700

    -

--- Machine_Learning/test.py
+++ Machine_Learning/test.py
@@ -1,2 +1,8 @@
-import matplotlib.pyplot as plt
-print(plt)
+import tensorflow as tf
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+if tf.config.list_physical_devices('GPU'):
+    print("TensorFlow is using GPU")
+else:
+    print("TensorFlow is using CPU")
