d6d7e6e9ee2caee1556edacfcf6d5fec9d1c0f1b
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-03-28 22:57:03 +0700

    -

--- Open_cv_detect_dog_cat/Code/2.train_model_yolo.py
+++ Open_cv_detect_dog_cat/Code/2.train_model_yolo.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 # Train the model
-model.train(data="dataset/data.yaml", epochs=50, imgsz=640, batch=16)
+model.train(data=r"D:\Important files Nannaphat\coding\Project\Open_cv_detect_dog_cat\train\data.yml", epochs=50, imgsz=640, batch=16)
 
 # Save the trained model
 model.export(format="onnx")  # Export to ONNX for deployment (optional)
