44c03e8c757f7e2ff83edea9d64d10da7f70e3e5
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-04-01 22:17:48 +0700

    -

---
+++ Open_cv_detect_dog_cat/Code/3.test_model_yolo.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+import cv2
+
+# Load the trained model
+model = YOLO(r"D:\Important files Nannaphat\coding\Project\runs\detect\train\weights\best.pt")
+
+# Read image
+img_path = r"D:\Important files Nannaphat\coding\Project\Machine_Learning\python+opencv\learn\image.jpg"
+image = cv2.imread(img_path)
+
+# Detect objects
+results = model(image)
+
+# Show results
+for r in results:
+    r.show()  # Display image with bounding boxes
