bbccdd2b86a264370fea6104e9b7861642ba98e0
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-05-06 20:30:40 +0700

    -

--- Open_cv_detect_dog_cat/Code/0.test.py
+++ Open_cv_detect_dog_cat/Code/0.test.py
@@ -21,3 +21,17 @@ char_dict = caer.sort_dict(char_dict, descending=True)
 characters = []
 for i in char_dict:
     characters.append(i[0])
+    
+for i in characters:
+    path = os.path.join(char_path, i)
+    for j in os.listdir(path):
+        pic_path = os.path.join(path, j)
+        picture = cv2.imread(pic_path)
+        if picture is None:
+            cv2.imshow("error : ", pic_path)
+            
+train = caer.preprocess_from_dir(DIR=char_path, classes=characters, IMG_SIZE=picture_size, channels=channels, isShuffle=True)
+
+feature_set, labels = caer.sep_train(train, IMG_SIZE=picture_size)
+feature_set = caer.normalize(feature_set)
+labels = to_categorical(labels, len(characters))
