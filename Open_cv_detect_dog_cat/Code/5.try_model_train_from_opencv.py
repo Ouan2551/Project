5ca9bc6ecb3f31510359716595b8a38aeffe656e
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-06-11 18:17:34 +0700

    -

--- Open_cv_detect_dog_cat/Code/5.try_model_train_from_opencv.py
+++ Open_cv_detect_dog_cat/Code/5.try_model_train_from_opencv.py
@@ -3,15 +3,15 @@ import numpy as np
 from tensorflow.keras.models import load_model #type: ignore
 
 # load model from patch
-loaded_model = load_model(r"C:\Important files Nannaphat\coding\Project\Open_cv_detect_dog_cat\model\dog_cat_model2.h5")
+loaded_model = load_model(r"C:\Important files Nannaphat\coding\Project\student_chk\model\detect_people_model1.h5")
 
 # output to check model
 loaded_model.summary()
 
 # class
-characters = ["Cat", "Dog"]
+characters = ["Boeing", "Ouan", "Third"]
 image_size = (80, 80)
-img = cv2.imread(r"C:\Important files Nannaphat\coding\Project\Open_cv_detect_dog_cat\dataset_train\train\images\66a483b2-8.jpg")
+img = cv2.imread(r"C:\Important files Nannaphat\coding\Project\student_chk\DATA\Ouan\2.jpg")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img = cv2.resize(img, image_size)  # resize image
 img = img.reshape(-1, image_size[0], image_size[1], 1)  # reshape image
